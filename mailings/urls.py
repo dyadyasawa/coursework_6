db225b9569b080f6b36210f69d7092a9b7b447a2
dyadyasawa <dyadyasawa@gmail.com>
2024-05-07 23:58:39 +0300

    Пробы.

--- mailings/urls.py
+++ mailings/urls.py
@@ -4,10 +4,12 @@ from django.views.decorators.cache import cache_page
 from django.urls import path
 
 from mailings.apps import MailingsConfig
+from mailings.views import StartPageView
 
 app_name = MailingsConfig.name
 
 
 urlpatterns = [
+    path('', StartPageView.as_view(), name='start_page'),
 
 ]
