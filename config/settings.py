d88f99d4908bb730e34e5018cfd76bfda47ddcc1
dyadyasawa <dyadyasawa@gmail.com>
2024-05-10 16:26:48 +0300

    Реализация механизма CRUD для рассылок.

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coursework_6_db',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        'PASSWORD': 'paragWay_38'
     }
 }
 
