5aeac6d0bc351783cdb3af15a99d23803c6247fc
dyadyasawa <dyadyasawa@gmail.com>
2024-05-24 17:41:38 +0300

    Создание и применение миграций.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
